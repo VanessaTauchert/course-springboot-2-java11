d82592323729a825c3655216298e022ff467363f
Vanessa <tauchertvanessa@gmail.com>
2022-09-02 11:01:52 -0300

    Order, Instant, ISO 8601 part 2

--- src/main/java/com/projetospring/course/config/TestConfig.java
+++ src/main/java/com/projetospring/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.projetospring.course.config;
 
+import com.projetospring.course.entities.Order;
 import com.projetospring.course.entities.User;
+import com.projetospring.course.repositories.OrderRepository;
 import com.projetospring.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -23,6 +29,12 @@ public class TestConfig implements CommandLineRunner{
         User u1 = new User(null, "Vanessa", "teste1@teste.com", "99999999", "123456");
         User u2 = new User(null, "Kerstin", "teste2@teste.com", "88888888", "123456");
 
+        Order o1 = new Order(null, Instant.parse("2019-01-20T00:00:00Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2020-06-20T00:00:00Z"), u2);
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2));
+
+
     }
 }
